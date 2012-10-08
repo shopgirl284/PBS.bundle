7789310fa01924a21171a82bfd8e7c63fcfb03d5
Mike <mike.d.meier@gmail.com>
2012-10-08 11:29:57 -0600

    v2.1 rewrite with CoveAPI support and MP4 video
    Conflicts:
    	Contents/Code/__init__.py
    	Contents/Site Configurations/pbs.xml

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,134 +1,80 @@
-PBS_URL = 'http://video.pbs.org/'
-PAGE_SIZE = 12
+PBS 		= SharedCodeService.CoveAPI.connect
+SortThumbs	= SharedCodeService.SortThumbs.SortThumbs
+API_ID		= SharedCodeService.PBS_API.API_ID
+API_SECRET	= SharedCodeService.PBS_API.API_SECRET
 
-# Used for platforms that do not have the Helper decrypt binary
-PBS_WEB_VIDEO_PREFIX = 'http://www-tc.pbs.org/s3/pbs.videoportal-prod.cdn/media/swf/PBSPlayer.swf?embed=true&start=0&width=512&height=288&reporting_url=&session_id=-&video=http://video.pbs.org/videoPlayerInfo/%s'
+PBS_PREFIX      = "/video/pbs"
+CACHE_INTERVAL  = 3600 * 3
+PBS_URL         = 'http://video.pbs.org/'
+PBS_VIDEO_URL   = 'http://video.pbs.org/video/%s'
+PAGE_SIZE  		= 12
+
+COVEAPI_HOST 	= 'http://api.pbs.org'
+ALL_PROGRAMS 	= '/cove/v1/programs/?fields=associated_images'
+ALL_EPISODES	= '/cove/v1/videos/?fields=associated_images&filter_availability_status=Available&filter_program=%s&filter_type=Episode&order_by=-airdate'
+
+NAMESPACES = {'a':'http://www.w3.org/2001/SMIL20/Language'}
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler('/video/pbs', VideoMenu, 'PBS', 'icon-default.png', 'art-default.jpg')
-  Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
-  MediaContainer.title1 = 'PBS'
-  MediaContainer.content = 'Items'
-  MediaContainer.art = R('art-default.jpg')
-  DirectoryItem.thumb = R('icon-default.png')
-  HTTP.CacheTime = CACHE_1HOUR * 3
+  Plugin.AddPrefixHandler(PBS_PREFIX, VideoMenu, 'PBS', 'icon-default.png', 'art-default.jpg')
+  ObjectContainer.title1 = 'PBS'
+  ObjectContainer.art = R('art-default.jpg')
+  DirectoryObject.thumb = R("icon-default.png")
+  HTTP.CacheTime = CACHE_INTERVAL
 
 ####################################################################################################
+@route('/video/pbs/videomenu')
 def VideoMenu():
-  dir = MediaContainer(noCache=True)
-  dir.Append(Function(DirectoryItem(GetPrograms, title="All Programs"), cls='subnav hide threecol'))
-  dir.Append(Function(DirectoryItem(GetPrograms, title="All Topics"), cls='subnav hide twocol', prefix="li[@class='topics-nav']/", path='li/ol/li/a/text()[normalize-space(.)]/parent::a'))
-  dir.Append(Function(DirectoryItem(GetMostWatched, title="Most Watched")))  
-  dir.Append(Function(InputDirectoryItem(Search, title=L("Search..."), prompt=L("Search for Videos"), thumb=S('icon-search.png'))))
-  return dir
+  oc = ObjectContainer(no_cache=True)
+  oc.add(DirectoryObject(key=Callback(GetPrograms), title=L('All Programs')))
+  oc.add(DirectoryObject(key=Callback(GetMostWatched), title=L('Most Watched')))
+  oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.pbs", title=L("Search..."), prompt=L("Search for Videos"), thumb=R('search.png')))
+  return oc
 
 ####################################################################################################
-def GetPrograms(sender, cls, prefix='', path='li/ol/li/a'):
-  dir = MediaContainer(title2=sender.itemTitle)
-  Log("//%sul[@class='%s']/%s" % (prefix, cls, path))
-  for program in HTML.ElementFromURL(PBS_URL).xpath("//%sul[@class='%s']/%s" % (prefix, cls, path)):
-    url = program.get('href')
-    dir.Append(Function(DirectoryItem(GetEpisodes, title=program.text), pid=url))
-  return dir
-  
+@route('/video/pbs/programs')
+def GetPrograms():
+  oc = ObjectContainer(title2=L('All Programs'))
+  programs = PBS(String.Decode(API_ID), String.Decode(API_SECRET)).programs.get(ALL_PROGRAMS)
+  for program in programs['results']:
+    thumbs = SortThumbs(program['associated_images'])
+    title = program['title']
+    tagline = program['short_description']
+    summary = program['long_description']
+    uri = program['resource_uri']
+    oc.add(DirectoryObject(key=Callback(GetEpisodes, uri=uri, title=title), title=title, tagline=tagline, summary=summary, thumb=Resource.ContentsOfURLWithFallback(url=thumbs, fallback='icon-default.png')))
+  return oc
+
 ####################################################################################################
-def GetMostWatched(sender):
-  dir = MediaContainer(title2=sender.itemTitle)
+@route('/video/pbs/episodes')
+def GetEpisodes(uri, title='Episodes', page=1):
+  oc = ObjectContainer(title2=title)
+  show_id = uri.split('/')[-2]
+  videos = PBS(String.Decode(API_ID), String.Decode(API_SECRET)).programs.get(ALL_EPISODES % show_id)
+  for video in videos['results']:
+    thumbs = SortThumbs(video['associated_images'])
+    airdate = video['airdate']
+    summary = video['long_description']
+    title = video['title']
+    uri = video['resource_uri']
+    tp_id = video['tp_media_object_id']
+    oc.add(VideoClipObject(url=PBS_VIDEO_URL % tp_id, title=title, summary=summary, originally_available_at=Datetime.ParseDate(airdate).date(),
+      thumb=Resource.ContentsOfURLWithFallback(url=thumbs, fallback='icon-default.png')))
+  
+  return oc
+
+####################################################################################################
+@route('/video/pbs/mostwatched')
+def GetMostWatched():
+  oc = ObjectContainer(title2=L("Most Watched"))
   for show in HTML.ElementFromURL(PBS_URL).xpath('//div[@id="most-watched-videos"]//li'):
     title = show.xpath('.//span[@class="title clear clearfix"]/a')[0].text
-    subtitle = show.xpath('.//span[@class="description"]')[0].text.strip()
-    summary = ''
+    summary = show.xpath('.//span[@class="description"]')[0].text.strip()
     thumb = show.xpath('.//img')[0].get('src')
     href = show.xpath('.//div/a')[0].get('href')
-    sid = href.split('/')[-2]
-    if PlatformCanDecrypt():
-        dir.Append(Function(VideoItem(PlayVideo, title, subtitle, summary, None, thumb), sid=sid))
-    else:
-        dir.Append(WebVideoItem(PBS_WEB_VIDEO_PREFIX % sid, title, subtitle, summary, None, thumb))
-    return dir
+    oc.add(VideoClipObject(url=href, title=title, summary=summary, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
+  
+  return oc
   
 ####################################################################################################
-def GetEpisodes(sender, pid, page=1):
-  dir = MediaContainer(viewGroup='InfoList', title2=sender.itemTitle)
-  url = pid
-  data = HTML.ElementFromURL(url)
-  for episode in data.xpath('//div[@id="fullepisodes"]//div[@class="videobox "]'):
-     title = episode.xpath('.//h2[@class="title"]')[0].text
-     try: subtitle = episode.xpath('.//span[@class="airdate"]')[0].text
-     except: subtitle = ''
-     summary = episode.xpath('.//span[@class="description"]')[0].text
-     duration = episode.xpath('.//span[@class="duration"]')[0].text
-     duration = [int(d) for d in duration.split(':')]
-     if len(duration) == 3:
-        duration = duration[0]*3600+duration[1]*60+duration[2]
-     else:
-        duration = duration[0]*60+duration[1]
-     duration = duration * 1000
-     thumb = episode.xpath('.//img[@class="thumbnail"]')[0].get('src')
-     sid = episode.xpath('.//input[@class="contentID"]')[0].get('value')
-     if PlatformCanDecrypt():
-        dir.Append(Function(VideoItem(PlayVideo, title, subtitle, summary, duration, thumb), sid=sid))
-     else:
-        dir.Append(WebVideoItem(PBS_WEB_VIDEO_PREFIX % sid, title, subtitle, summary, duration, thumb))
-  return dir
-  
-####################################################################################################
-def Search(sender, query, page=1):
-  dir = MediaContainer(viewGroup='InfoList', title2='Search Results', replaceParent=(page>1))
-  query = query.replace(' ', '+')
-  data = HTML.ElementFromURL('http://www.pbs.org/search/?q=%s&mediatype=Video&start=%d' % (query, (int(page)-1)*10))
-  for show in data.xpath('//div[@class="ez-mod ez-itemMod ez-mainSearch ez-col-1"]//li'):
-    try:
-      show_title = ''.join(show.xpath('.//p[@class="ez-metaextra1 ez-icon"]//text()'))
-      ep_title = ''.join(show.xpath('.//a[@class="ez-title"]//text()'))
-      title = '%s | %s' % (show_title, ep_title) 
-      summary = ''.join(show.xpath('.//p[@class="ez-desc"]//text()'))
-      thumb = show.xpath('.//img[@class="ez-primaryThumb"]')[0].get('src')
-      sid = show.xpath('.//a[@class="ez-title"]')[0].get('href').split('/')[-1]
-      if PlatformCanDecrypt():
-        dir.Append(Function(VideoItem(PlayVideo, title, summary=summary, thumb=thumb), sid=sid))
-      else:
-        dir.Append(WebVideoItem(PBS_WEB_VIDEO_PREFIX % sid, title, summary=summary, thumb=thumb))
-    except:
-      pass
-  if len(data.xpath('//a[@title="Next Result Page"]')) != 0:
-    dir.Append(Function(DirectoryItem(Search, title="More results"), query=query, page=page+1))
-  return dir
-
-####################################################################################################
-def PlatformCanDecrypt():
-    if Platform.OS == "MacOSX" or Platform.OS == "Linux":
-        return True
-    else:
-        return False
-  
-####################################################################################################
-def ExtractReleaseUrlNative(releaseUrl):
-	Log("ReleaseURL:"+releaseUrl)
-	tokens = releaseUrl.split('$')
-	decrypted = Helper.Run('decrypt', tokens[1] + "#" + tokens[2])
-	decrypted = decrypted + "&format=SMIL"
-	Log("Decrypted URL:"+decrypted)
-	return decrypted
-
-####################################################################################################
-def PlayVideo(sender, sid):
-  url = PBS_URL + 'videoPlayerInfo/%s' % sid
-  
-  xml = HTTP.Request(url, cacheTime=0).content
-  #xml = String.Unquote(xml, False)
-  start = 12 + xml.find('<releaseURL>')
-  end = xml.find('</releaseURL>')
-  release = xml[start:end]
-  releaseUrl = ExtractReleaseUrlNative(release)
-  NS = {"ns":"http://www.w3.org/2001/SMIL20/Language", "tp":"http://xml.theplatform.com/mps/metadata/content/custom"}
-  xml = XML.ElementFromURL(releaseUrl, cacheTime=0)
-  player = xml.xpath('/ns:smil/ns:head/ns:meta', namespaces=NS)[0].get('base')
-  directFeed = player.find('http://') > -1
-  if directFeed: 
-    clip = xml.xpath('/ns:smil/ns:body//ns:ref', namespaces=NS)[0].get('src')
-  else: 
-    clip = 'mp4:'+xml.xpath('/ns:smil/ns:body//ns:ref', namespaces=NS)[0].get('src').replace('.mp4','')
-  return Redirect(RTMPVideoItem(player, clip))
-  
